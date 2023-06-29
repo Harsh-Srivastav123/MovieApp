41a56ded3fee77a3e8369a0f6078285e0754e087
Harsh-Srivastav123 <114308429+Harsh-Srivastav123@users.noreply.github.com>
2023-06-29 18:06:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/movieapp/navigation/MovieNavigation.kt
@@ -0,0 +1,27 @@
+package com.example.movieapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.movieapp.screens.DetailScreen
+import com.example.movieapp.screens.HomeScreen
+
+@Composable
+fun MovieNavigation(){
+    val navController= rememberNavController()
+    NavHost(navController = navController, startDestination =MovieScreens.HomeScreen.name){
+        composable(MovieScreens.HomeScreen.name){
+            HomeScreen(navController = navController)
+        }
+        composable(MovieScreens.DetailScreen.name+"/{movie}",
+        arguments = listOf(navArgument(name = "movie"){
+            type= NavType.StringType
+        })
+        ){
+            DetailScreen(navController = navController,it.arguments?.getString("movie"))
+        }
+    }
+}
